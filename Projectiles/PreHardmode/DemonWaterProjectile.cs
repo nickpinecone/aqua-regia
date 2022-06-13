416412194ccfd24d35293b1bcf7db2605e2df8ac
threedots <threedotsellipsis@protonmail.com>
2022-06-14 00:29:50 +0500

    Fixed the recursive projectile spawning issue

--- Projectiles/PreHardmode/DemonWaterProjectile.cs
+++ Projectiles/PreHardmode/DemonWaterProjectile.cs
@@ -1,5 +1,6 @@
 using Microsoft.Xna.Framework;
 using Terraria;
+using Terraria.DataStructures;
 using Terraria.ID;
 using Terraria.ModLoader;
 
@@ -15,14 +16,30 @@ namespace WaterGuns.Projectiles.PreHardmode
 
         public override void OnHitNPC(NPC target, int damage, float knockback, bool crit)
         {
-            int direction = Main.MouseWorld.X - Projectile.position.X > 0 ? 1 : -1;
+            // Changing the height in order to avoid recursive calls
+            if (Projectile.height == 8)
+            {
+                int direction = Main.MouseWorld.X - Projectile.position.X > 0 ? 1 : -1;
 
-            var modifiedVelocity = new Vector2(1 * direction, 0).RotatedBy(MathHelper.ToRadians(225 * -direction)).RotatedByRandom(MathHelper.ToRadians(5));
-            modifiedVelocity *= 10;
-            var offset = new Vector2(Projectile.position.X + 128 * direction, Projectile.position.Y - 128);
+                var modifiedVelocity = new Vector2(1 * direction, 0).RotatedBy(MathHelper.ToRadians(225 * -direction)).RotatedByRandom(MathHelper.ToRadians(5));
+                // Speed it up a bit
+                modifiedVelocity *= 10;
+                var offset = new Vector2(Projectile.position.X + 196 * direction, Projectile.position.Y - 196);
 
-            Projectile.NewProjectile(Projectile.GetSource_FromThis(), offset, modifiedVelocity, Projectile.type, Projectile.damage, Projectile.knockBack);
+                Projectile proj = Projectile.NewProjectileDirect(Projectile.GetSource_FromThis(), offset, modifiedVelocity, Projectile.type, Projectile.damage, Projectile.knockBack - 1, Projectile.owner);
+                proj.height -= 1;
+            }
             base.OnHitNPC(target, damage, knockback, crit);
+        }
+
+        float gravity = 0.001f;
+        public override void AI()
+        {
+            // Curve it like the in-game water gun projectile
+            gravity += 0.002f;
+            Projectile.velocity.Y += gravity;
+
+            base.AI();
         }
     }
 }
