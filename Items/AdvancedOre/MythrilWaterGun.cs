7e3f0e2445b47018f70e5fc0d0d9cae631de44fa
threedots <threedotsellipsis@protonmail.com>
2022-06-16 15:21:50 +0500

    Refactor to advanced-ore water guns

--- Items/AdvancedOre/MythrilWaterGun.cs
+++ Items/AdvancedOre/MythrilWaterGun.cs
@@ -6,35 +6,15 @@ using Terraria.ModLoader;
 
 namespace WaterGuns.Items.AdvancedOre
 {
-    public class MythrilWaterGun : ModItem
+    public class MythrilWaterGun : BaseWaterGun
     {
-        public override void SetStaticDefaults()
-        {
-            // DisplayName.SetDefault("BasicSword"); // By default, capitalization in classnames will add spaces to the display name. You can customize the display name here by uncommenting this line.
-        }
 
         public override void SetDefaults()
         {
-            Item.CloneDefaults(ItemID.WaterGun);
+            base.SetDefaults();
 
             Item.damage = 36;
             Item.knockBack = 4;
-            Item.shoot = ModContent.ProjectileType<Projectiles.AdvancedOre.AdvancedWaterProjectile>();
-            Item.useTime -= 2;
-        }
-
-        public override bool Shoot(Player player, EntitySource_ItemUse_WithAmmo source, Vector2 position, Vector2 velocity, int type, int damage, float knockback)
-        {
-            // Make it a little inaccurate
-            Vector2 modifiedVelocity = velocity.RotatedByRandom(MathHelper.ToRadians(1));
-            Projectile.NewProjectile(source, position, modifiedVelocity, type, damage, knockback, player.whoAmI);
-
-            return false;
-        }
-
-        public override Vector2? HoldoutOffset()
-        {
-            return new Vector2(0, 4);
         }
 
         public override void AddRecipes()
--- Items/AdvancedOre/TitaniumWaterGun.cs
+++ Items/AdvancedOre/TitaniumWaterGun.cs
@@ -6,22 +6,19 @@ using Terraria.ModLoader;
 
 namespace WaterGuns.Items.AdvancedOre
 {
-    public class TitaniumWaterGun : ModItem
+    public class TitaniumWaterGun : BaseWaterGun
     {
         public override void SetStaticDefaults()
         {
-            // DisplayName.SetDefault("BasicSword"); // By default, capitalization in classnames will add spaces to the display name. You can customize the display name here by uncommenting this line.
             Tooltip.SetDefault("Shoots two projecitles in opposite directions");
         }
 
         public override void SetDefaults()
         {
-            Item.CloneDefaults(ItemID.WaterGun);
+            base.SetDefaults();
 
             Item.damage = 40;
             Item.knockBack = 4;
-            Item.shoot = ModContent.ProjectileType<Projectiles.AdvancedOre.AdvancedWaterProjectile>();
-            Item.useTime -= 2;
         }
 
         public override bool Shoot(Player player, EntitySource_ItemUse_WithAmmo source, Vector2 position, Vector2 velocity, int type, int damage, float knockback)
@@ -35,11 +32,6 @@ namespace WaterGuns.Items.AdvancedOre
             Projectile.NewProjectile(source, secondPosition, secondVelocity, type, damage, knockback, player.whoAmI);
 
             return false;
-        }
-
-        public override Vector2? HoldoutOffset()
-        {
-            return new Vector2(0, 4);
         }
 
         public override void AddRecipes()
