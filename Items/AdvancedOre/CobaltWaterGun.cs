acbb4e2bbd9327bf418eaa579707d966cb395fe9
threedots <threedotsellipsis@protonmail.com>
2022-06-16 13:59:47 +0500

    Added stubs for advanced-ore water guns

---
+++ Items/AdvancedOre/CobaltWaterGun.cs
@@ -0,0 +1,36 @@
+using Microsoft.Xna.Framework;
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace WaterGuns.Items.BasicOre
+{
+    public class CobaltWaterGun : ModItem
+    {
+        public override void SetStaticDefaults()
+        {
+            // DisplayName.SetDefault("BasicSword"); // By default, capitalization in classnames will add spaces to the display name. You can customize the display name here by uncommenting this line.
+        }
+
+        public override void SetDefaults()
+        {
+            Item.CloneDefaults(ItemID.WaterGun);
+
+            Item.damage = 25;
+            Item.knockBack = 4;
+        }
+
+        public override Vector2? HoldoutOffset()
+        {
+            return new Vector2(0, 4);
+        }
+
+        public override void AddRecipes()
+        {
+            Recipe recipe = CreateRecipe();
+            recipe.AddIngredient(ItemID.CobaltBar, 18);
+            recipe.AddTile(TileID.Anvils);
+            recipe.Register();
+        }
+    }
+}
