8b57abbe33cfb2725ff553cfb0b946dfe4bd2b80
threedots <threedotsellipsis@protonmail.com>
2022-06-19 17:28:56 +0500

    Added turret ability to mini water gun

--- Items/Hardmode/MiniWaterGun.cs
+++ Items/Hardmode/MiniWaterGun.cs
@@ -25,10 +25,30 @@ namespace WaterGuns.Items.Hardmode
             Item.shoot = ModContent.ProjectileType<Projectiles.Hardmode.MiniWaterProjectile>();
         }
 
+        bool turretMode = false;
+        Projectile turret = null;
+        public override bool AltFunctionUse(Player player)
+        {
+            turretMode = !turretMode;
+            if (turretMode)
+            {
+                turret = Projectile.NewProjectileDirect(Projectile.GetSource_NaturalSpawn(), player.position, Vector2.Zero, ModContent.ProjectileType<Projectiles.Hardmode.TurretWaterProjectile>(), 0, 0, player.whoAmI);
+            }
+            else
+            {
+                turret.Kill();
+            }
+            return base.AltFunctionUse(player);
+        }
+
         public override bool Shoot(Player player, EntitySource_ItemUse_WithAmmo source, Vector2 position, Vector2 velocity, int type, int damage, float knockback)
         {
-            Vector2 modifiedVelocity = velocity.RotatedByRandom(MathHelper.ToRadians(8));
-            Projectile.NewProjectile(source, position, modifiedVelocity, type, damage, knockback, player.whoAmI);
+            if (!turretMode)
+            {
+                Vector2 modifiedVelocity = velocity.RotatedByRandom(MathHelper.ToRadians(8));
+                var offset = new Vector2(position.X + velocity.X * 4, position.Y + velocity.Y * 4);
+                Projectile.NewProjectile(source, offset, modifiedVelocity, type, damage, knockback, player.whoAmI);
+            }
             return false;
         }
 
--- Projectiles/Hardmode/MiniWaterProjectile.cs
+++ Projectiles/Hardmode/MiniWaterProjectile.cs
@@ -3,9 +3,43 @@ using Terraria;
 using Terraria.DataStructures;
 using Terraria.ID;
 using Terraria.ModLoader;
+using System;
 
 namespace WaterGuns.Projectiles.Hardmode
 {
+    public class TurretWaterProjectile : ModProjectile
+    {
+        public override void SetDefaults()
+        {
+            Projectile.width = 8;
+            Projectile.height = 8;
+
+            Projectile.friendly = false;
+            Projectile.hostile = false;
+        }
+
+        int delayMax = 15;
+        int delay = 15;
+        bool turned = false;
+        public override void AI()
+        {
+            base.AI();
+
+            var distanceToMouse = new Vector2(Main.MouseWorld.X - Projectile.position.X, Main.MouseWorld.Y - Projectile.position.Y);
+            distanceToMouse.Normalize();
+            Projectile.rotation = Projectile.Center.AngleTo(Main.MouseWorld);
+
+            if (Main.mouseLeft && delay >= delayMax)
+            {
+                delay = 0;
+                var velocity = distanceToMouse * 10;
+                var offset = new Vector2(Projectile.position.X + MathF.Abs(velocity.X * 2), Projectile.position.Y + MathF.Abs(velocity.Y * 2)) + (Projectile.velocity * 1.5f).RotatedBy(90);
+                Projectile.NewProjectile(Projectile.GetSource_FromThis(), offset, velocity, ModContent.ProjectileType<AdvancedOre.AdvancedWaterProjectile>(), 40, 3, Projectile.owner);
+            }
+            delay += 1;
+        }
+    }
+
     public class MiniWaterProjectile : BaseProjectile
     {
         public override void SetDefaults()
