3cecf772de490c922c89e939dc08ec92d3270351
threedots <threedotsellipsis@protonmail.com>
2022-06-29 17:04:04 +0500

    Scope now works with all hardmode weapons

--- Items/Hardmode/IchorWaterGun.cs
+++ Items/Hardmode/IchorWaterGun.cs
@@ -24,6 +24,9 @@ namespace WaterGuns.Items.Hardmode
 
         public override bool Shoot(Player player, EntitySource_ItemUse_WithAmmo source, Vector2 position, Vector2 velocity, int type, int damage, float knockback)
         {
+            float inaccuracy = player.GetModPlayer<GlobalPlayer>().CalculateAccuracy(4);
+            float offsetInaccuracy = player.GetModPlayer<GlobalPlayer>().CalculateAccuracy(0.3f);
+
             // Put it above the mouse
             // Could create complications if zoomed out too much
             // Projectiles will not reach all the way to the bottom
@@ -33,13 +36,10 @@ namespace WaterGuns.Items.Hardmode
             // Speed them up a bit
             int projectileSpeed = 14;
 
-            // Chage accuracy
-            float inaccuracy = player.GetModPlayer<GlobalPlayer>().CalculateAccuracy(4);
-
             for (int i = 0; i < 4; i++)
             {
                 var modifiedVelocity = new Vector2(0, 1).RotatedByRandom(MathHelper.ToRadians(inaccuracy));
-                position.X = position.RotatedByRandom(MathHelper.ToRadians(0.4f)).X;
+                position.X = position.RotatedByRandom(MathHelper.ToRadians(offsetInaccuracy)).X;
                 modifiedVelocity *= projectileSpeed;
 
                 Projectile.NewProjectile(source, position, modifiedVelocity, type, damage, knockback, player.whoAmI);
