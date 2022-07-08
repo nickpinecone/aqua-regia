65955ab40ac90cd8b19c672571ded5e0308d1b19
threedots <threedotsellipsis@protonmail.com>
2022-07-08 21:09:07 +0500

    Finished a demo versoin of water gun summon

--- Projectiles/Special/WaterGunSummon.cs
+++ Projectiles/Special/WaterGunSummon.cs
@@ -3,6 +3,7 @@ using Terraria;
 using Terraria.DataStructures;
 using Terraria.ID;
 using Terraria.ModLoader;
+using System;
 
 namespace WaterGuns.Projectiles.Special
 {
@@ -30,32 +31,33 @@ namespace WaterGuns.Projectiles.Special
         int delay = 0;
         public override void AI()
         {
-
-            if (delay >= 10)
+            delay += 1;
+            if (delay >= 30)
             {
+                delay = 0;
 
-                float leastDist = 0;
+                float leastDist = float.PositiveInfinity;
                 var vector = Vector2.Zero;
                 for (int i = 0; i < Main.npc.Length; i++)
                 {
                     var dist = Projectile.Distance(Main.npc[i].position);
-                    if (i == 0)
+                    var distance = Main.player[Main.myPlayer].position - Main.npc[i].position;
+                    bool isVisible = Math.Abs(distance.X) < Main.ViewSize.X && Math.Abs(distance.Y) < Main.ViewSize.Y;
+
+                    if (Main.npc[i].life > 0 && isVisible && dist < leastDist)
                     {
                         leastDist = dist;
-                        vector = Projectile.position - Main.npc[i].position;
-                    }
-                    else if (dist < leastDist)
-                    {
-                        leastDist = dist;
-                        vector = Projectile.position - Main.npc[i].position;
+                        vector = Main.npc[i].position - Projectile.position;
                     }
                 }
 
-                Projectile.NewProjectile(Projectile.GetSource_FromThis(), Projectile.Center, vector * 10, ProjectileID.WaterGun, Projectile.damage, Projectile.knockBack, Projectile.owner);
-
-                delay = 0;
+                if (vector != Vector2.Zero)
+                {
+                    vector.Normalize();
+                    var offset = Projectile.Center + vector * 4;
+                    Projectile.NewProjectile(Projectile.GetSource_NaturalSpawn(), offset, vector * 10, ModContent.ProjectileType<Projectiles.Hardmode.WaterProjectile>(), Projectile.damage, Projectile.knockBack, Projectile.owner);
+                }
             }
-            delay += 1;
 
             base.AI();
         }
