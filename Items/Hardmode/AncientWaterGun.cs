526c8e02d81c87460d5829a5a60e3e236b41d742
threedots <threedotsellipsis@protonmail.com>
2022-07-17 20:25:37 +0500

    Redesigned the ancient water gun

--- Items/Hardmode/AncientWaterGun.cs
+++ Items/Hardmode/AncientWaterGun.cs
@@ -23,23 +23,28 @@ namespace WaterGuns.Items.Hardmode
             Item.shoot = ModContent.ProjectileType<Projectiles.Hardmode.WaterProjectile>();
         }
 
-        public override bool? UseItem(Player player)
-        {
-            return base.UseItem(player);
-        }
-
         public override bool Shoot(Player player, EntitySource_ItemUse_WithAmmo source, Vector2 position, Vector2 velocity, int type, int damage, float knockback)
         {
-            base.defaultInaccuracy = 10;
+            // Spawn projectiles from the ground solid block
+            base.defaultInaccuracy = 4;
             for (int i = 0; i < Main.npc.Length; i++)
             {
                 var distance = player.position - Main.npc[i].position;
                 bool isVisible = Math.Abs(distance.X) < Main.ViewSize.X && Math.Abs(distance.Y) < Main.ViewSize.Y;
                 if (Main.npc[i].life > 0 && isVisible)
                 {
-                    int rotation = Main.rand.Next(0, 360);
-                    var randomPosition = Main.npc[i].Center + new Vector2(256, 0).RotatedBy(MathHelper.ToRadians(rotation));
-                    var modifiedVelocity = new Vector2(10, 0).RotatedBy(MathHelper.ToRadians(rotation - 180));
+                    var randomPosition = Vector2.Zero;
+                    var rotation = Main.rand.Next(-12, 12);
+                    for (int k = 0; k < Main.ViewSize.Y; k += 16)
+                    {
+                        var tilePosition = (Main.npc[i].Center + new Vector2(0, k).RotatedBy(MathHelper.ToRadians(rotation))).ToTileCoordinates();
+                        if (Main.tile[tilePosition.X, tilePosition.Y].HasTile && Main.tile[tilePosition.X, tilePosition.Y].BlockType == BlockType.Solid)
+                        {
+                            randomPosition = tilePosition.ToWorldCoordinates() + new Vector2(0, 32).RotatedBy(MathHelper.ToRadians(rotation));
+                            break;
+                        }
+                    }
+                    var modifiedVelocity = new Vector2(0, -10).RotatedBy(MathHelper.ToRadians(rotation));
 
                     var proj = base.SpawnProjectile(player, source, randomPosition, modifiedVelocity, type, damage, knockback);
                     proj.tileCollide = false;
