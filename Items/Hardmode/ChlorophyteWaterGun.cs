7f62692c8c939c6163d7bedf89065596ff43cdca
threedots <threedotsellipsis@protonmail.com>
2023-05-17 19:05:33 +0500

    Full pump attack for chlorophyte gun

--- Items/Hardmode/ChlorophyteWaterGun.cs
+++ Items/Hardmode/ChlorophyteWaterGun.cs
@@ -3,6 +3,7 @@ using Terraria.ID;
 using Terraria.ModLoader;
 using Terraria.DataStructures;
 using Microsoft.Xna.Framework;
+using System;
 
 namespace WaterGuns.Items.Hardmode
 {
@@ -13,7 +14,7 @@ namespace WaterGuns.Items.Hardmode
             base.SetStaticDefaults();
 
             DisplayName.SetDefault("Chlorophyte Water Sprayer");
-            Tooltip.SetDefault("Chases after your foes and releases deadly spores");
+            Tooltip.SetDefault("Chases after your foes\nFull Pump: Releases Man Eaters that cling onto enemies");
         }
 
         public override Vector2? HoldoutOffset()
@@ -33,6 +34,56 @@ namespace WaterGuns.Items.Hardmode
             Item.useAnimation -= 8;
 
             Item.shoot = ModContent.ProjectileType<Projectiles.Hardmode.ChlorophyteWaterProjectile>();
+
+            base.increasePumpLevel = true;
+            base.maxPumpLevel = 12;
+        }
+
+        public override bool Shoot(Player player, EntitySource_ItemUse_WithAmmo source, Vector2 position, Vector2 velocity, int type, int damage, float knockback)
+        {
+            if (pumpLevel >= maxPumpLevel)
+            {
+                float detectRange = MathF.Pow(1200f, 2);
+
+                Vector2 vel = Vector2.Zero;
+                int count = 0;
+                for (int i = 0; i < Main.npc.Length; i++)
+                {
+                    if (count >= 3)
+                    {
+                        break;
+                    }
+
+                    NPC target = Main.npc[i];
+
+                    if (!target.CanBeChasedBy() || target.Center.DistanceSQ(player.Center) > detectRange)
+                    {
+                        continue;
+                    }
+
+
+                    vel = player.Center.DirectionTo(target.Center);
+                    vel.Normalize();
+                    vel *= 14;
+
+                    count += 1;
+
+                    Projectile.NewProjectile(source, position, vel, ModContent.ProjectileType<Projectiles.Hardmode.PlantClinger>(), (int)(damage / 1.6f), 0, player.whoAmI);
+                }
+
+                // Release three man eaters always
+                if (count != 0)
+                {
+                    for (int i = 0; i < 3 - count; i++)
+                    {
+                        Vector2 newVel = vel.RotatedByRandom(MathHelper.ToRadians(10f));
+                        Projectile.NewProjectile(source, position, newVel, ModContent.ProjectileType<Projectiles.Hardmode.PlantClinger>(), (int)(damage / 1.6f), 0, player.whoAmI);
+                    }
+                }
+            }
+
+
+            return base.Shoot(player, source, position, velocity, type, damage, knockback);
         }
 
         public override void AddRecipes()
--- Projectiles/Hardmode/ChlorophyteWaterProjectile.cs
+++ Projectiles/Hardmode/ChlorophyteWaterProjectile.cs
@@ -4,8 +4,142 @@ using Terraria.DataStructures;
 using Terraria.ID;
 using Terraria.ModLoader;
 
+using System;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+using Terraria.Audio;
+using Terraria.GameContent;
+using ReLogic.Content;
+
 namespace WaterGuns.Projectiles.Hardmode
 {
+    // Example Advanced Flail is a complete adaption of Ball O' Hurt projectile. The code has been rewritten a bit to make it easier to follow. Compare this code against the decompiled Terraria code for an example of adapting vanilla code. A few comments and extra code snippets show features from other vanilla flails as well.
+    // Example Advanced Flail shows a plethora of advanced AI and collision topics.
+    // See ExampleFlail for a simpler but less customizable flail projectile example.
+    public class PlantClinger : BaseProjectile
+    {
+        private const string ChainTexturePath = "WaterGuns/Projectiles/Hardmode/PlantClingerChain"; // The folder path to the flail chain sprite
+
+        public override void SetStaticDefaults()
+        {
+            // These lines facilitate the trail drawing
+            ProjectileID.Sets.TrailCacheLength[Projectile.type] = 6;
+            ProjectileID.Sets.TrailingMode[Projectile.type] = 2;
+            Main.projFrames[Projectile.type] = 3;
+        }
+
+        public override void SetDefaults()
+        {
+            Projectile.netImportant = true; // This ensures that the projectile is synced when other players join the world.
+            Projectile.width = 56; // The width of your projectile
+            Projectile.height = 46; // The height of your projectile
+            Projectile.friendly = true; // Deals damage to enemies
+            Projectile.penetrate = -1; // Infinite pierce
+            Projectile.usesIDStaticNPCImmunity = true; // Used for hit cooldown changes in the ai hook
+            Projectile.timeLeft = 160;
+            Projectile.tileCollide = false;
+
+            // Vanilla flails all use aiStyle 15, but the code isn't customizable so an adaption of that aiStyle is used in the AI method
+        }
+
+        NPC _target = null;
+        Vector2 hitPoint;
+        public override void OnHitNPC(NPC target, int damage, float knockback, bool crit)
+        {
+            base.OnHitNPC(target, damage, knockback, crit);
+
+            if (_target == null)
+            {
+                _target = target;
+
+                hitPoint = Projectile.Center - target.Center;
+            }
+        }
+
+        public override void AI()
+        {
+            base.AI();
+
+            if (_target != null)
+            {
+                Projectile.Center = _target.Center + hitPoint;
+
+                if (_target.GetLifePercent() <= 0f)
+                {
+                    _target = null;
+                }
+            }
+            else
+            {
+                AutoAim();
+            }
+
+            if (++Projectile.frameCounter >= 6)
+            {
+                Projectile.frameCounter = 0;
+                // Or more compactly Projectile.frame = ++Projectile.frame % Main.projFrames[Projectile.type];
+                if (++Projectile.frame >= Main.projFrames[Projectile.type])
+                    Projectile.frame = 0;
+            }
+
+            Projectile.rotation = Projectile.velocity.ToRotation() + MathHelper.Pi;
+        }
+
+        // PreDraw is used to draw a chain and trail before the projectile is drawn normally.
+        public override bool PreDraw(ref Color lightColor)
+        {
+            Vector2 playerArmPosition = Main.GetPlayerArmPosition(Projectile);
+
+            // This fixes a vanilla GetPlayerArmPosition bug causing the chain to draw incorrectly when stepping up slopes. The flail itself still draws incorrectly due to another similar bug. This should be removed once the vanilla bug is fixed.
+            playerArmPosition.Y -= Main.player[Projectile.owner].gfxOffY;
+
+            Asset<Texture2D> chainTexture = ModContent.Request<Texture2D>(ChainTexturePath);
+
+            Rectangle? chainSourceRectangle = null;
+            // Drippler Crippler customizes sourceRectangle to cycle through sprite frames: sourceRectangle = asset.Frame(1, 6);
+            float chainHeightAdjustment = 0f; // Use this to adjust the chain overlap. 
+
+            Vector2 chainOrigin = chainSourceRectangle.HasValue ? (chainSourceRectangle.Value.Size() / 4f) : (chainTexture.Size() / 4f);
+            Vector2 chainDrawPosition = Projectile.Center;
+            Vector2 vectorFromProjectileToPlayerArms = playerArmPosition.MoveTowards(chainDrawPosition, 4f) - chainDrawPosition;
+            Vector2 unitVectorFromProjectileToPlayerArms = vectorFromProjectileToPlayerArms.SafeNormalize(Vector2.Zero);
+            float chainSegmentLength = (chainSourceRectangle.HasValue ? chainSourceRectangle.Value.Height : chainTexture.Height()) + chainHeightAdjustment;
+            if (chainSegmentLength == 0)
+            {
+                chainSegmentLength = 10; // When the chain texture is being loaded, the height is 0 which would cause infinite loops.
+            }
+            float chainRotation = unitVectorFromProjectileToPlayerArms.ToRotation() + MathHelper.PiOver2;
+            int chainCount = 0;
+            float chainLengthRemainingToDraw = vectorFromProjectileToPlayerArms.Length() + chainSegmentLength / 2f;
+
+            // This while loop draws the chain texture from the projectile to the player, looping to draw the chain texture along the path
+            while (chainLengthRemainingToDraw > 0f)
+            {
+                // This code gets the lighting at the current tile coordinates
+                Color chainDrawColor = Lighting.GetColor((int)chainDrawPosition.X / 16, (int)(chainDrawPosition.Y / 16f));
+
+                // Flaming Mace and Drippler Crippler use code here to draw custom sprite frames with custom lighting.
+                // Cycling through frames: sourceRectangle = asset.Frame(1, 6, 0, chainCount % 6);
+                // This example shows how Flaming Mace works. It checks chainCount and changes chainTexture and draw color at different values
+
+                var chainTextureToDraw = chainTexture;
+
+                // Here, we draw the chain texture at the coordinates
+                Main.spriteBatch.Draw(chainTextureToDraw.Value, chainDrawPosition - Main.screenPosition, chainSourceRectangle, chainDrawColor, chainRotation, chainOrigin, 1f, SpriteEffects.None, 0f);
+
+                // chainDrawPosition is advanced along the vector back to the player by the chainSegmentLength
+                chainDrawPosition += unitVectorFromProjectileToPlayerArms * chainSegmentLength;
+                chainCount++;
+                chainLengthRemainingToDraw -= chainSegmentLength;
+            }
+
+            return true;
+        }
+    }
+
     public class ChlorophyteWaterProjectile : BaseProjectile
     {
         public override void SetDefaults()
@@ -16,31 +150,11 @@ namespace WaterGuns.Projectiles.Hardmode
             Projectile.timeLeft += 20;
         }
 
-        public override void OnSpawn(IEntitySource source)
-        {
-            base.OnSpawn(source);
-
-            data.dustScale = 1;
-        }
-
-        int delay = 0;
-        int delayMax = Main.rand.Next(0, 40);
         public override void AI()
         {
             base.AI();
             base.CreateDust();
             base.AutoAim();
-
-            delay += 1;
-            if (delay >= delayMax)
-            {
-                delay = 0;
-                delayMax = Main.rand.Next(30, 80);
-
-                var position = Projectile.Center + new Vector2(Main.rand.Next(-30, 30), Main.rand.Next(-30, 30));
-                Projectile.NewProjectile(Projectile.GetSource_NaturalSpawn(), position, Vector2.Zero, ProjectileID.SporeTrap, Projectile.damage / 2, Projectile.knockBack, Projectile.owner);
-            }
-
         }
     }
 }
