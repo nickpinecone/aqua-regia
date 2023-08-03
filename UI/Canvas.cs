2631f301b712f5e7f785e4595330993ec4fd0341
threedots <threedotsellipsis@protonmail.com>
2023-08-03 18:00:56 +0500

    Fix ui not appearing

--- UI/Canvas.cs
+++ UI/Canvas.cs
@@ -15,16 +15,18 @@ namespace WaterGuns.UI
             panel = new UIPanel();
             panel.Width.Set(10, 0);
             panel.Height.Set(10, 0);
-            position = new Vector2(Main.screenWidth + 20, Main.screenHeight - 10);
-            panel.Top.Set(position.Y, 0);
-            panel.Left.Set(position.X, 0);
             Append(panel);
         }
 
         public override void Update(GameTime gameTime)
         {
+
             if (Main.player[Main.myPlayer].HeldItem.ModItem is Items.CommonWaterGun gun)
             {
+                position = new Vector2(Main.screenWidth - 60, Main.screenHeight - 60);
+                panel.Top.Set(position.Y, 0);
+                panel.Left.Set(position.X, 0);
+
                 panel.BorderColor = new Color(255, 255, 255, 0);
                 panel.BackgroundColor = new Color(255, 255, 255, 0);
                 panel.SetPadding(1f);
