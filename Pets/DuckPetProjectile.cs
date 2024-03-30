a34ed91f1b361a7aaed2e3fd90d25d093962efec
threedots <threedotsellipsis@protonmail.com>
2024-03-30 13:42:04 +0500

    Change duck character preview

--- Pets/DuckPetProjectile.cs
+++ Pets/DuckPetProjectile.cs
@@ -20,8 +20,9 @@ namespace WaterGuns.Pets
             // * To stop the animation if the player is not highlighted /is standing, as done by most grounded pets, add a.WhenNotSelected(0, 0)(you can customize it just like SimpleLoop)
             // * To set offset and direction, use .WithOffset(x, y) and.WithSpriteDirection(-1)
             // * To further customize the behavior and animation of the pet(as its AI does not run), you have access to a few vanilla presets in DelegateMethods.CharacterPreview to use via .WithCode().You can also make your own, showcased in MinionBossPetProjectile
-            ProjectileID.Sets.CharacterPreviewAnimations[Projectile.type] = ProjectileID.Sets.SimpleLoop(3, 8, 6)
-                .WithOffset(-10, -20f)
+            ProjectileID.Sets.CharacterPreviewAnimations[Projectile.type] = ProjectileID.Sets.SimpleLoop(3, 8, 4)
+                .WhenNotSelected(0, 0)
+                .WithOffset(-15, 0f)
                 .WithSpriteDirection(-1)
                 .WithCode(DelegateMethods.CharacterPreview.BerniePet);
         }
