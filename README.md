b5368b2af158f7860331063ab6f3bc0c873ff30e
threedots <threedotsellipsis@protonmail.com>
2024-06-10 23:07:09 +0500

    Initiate rewrite of the mod

--- README.md
+++ README.md
@@ -4,5 +4,3 @@ A Terraria mod that brings more water guns to the game that actually do damage
 [Discord server](https://discord.gg/hEcs7ynpee)
 
 [Steam workshop](https://steamcommunity.com/sharedfiles/filedetails/?id=2827232760)
-
-Water Gun sound effects obtained from https://www.zapsplat.com
--- WaterGuns.cs
+++ WaterGuns.cs
@@ -1,40 +1,8 @@
 using Terraria.ModLoader;
-using Terraria.Localization;
-using Terraria.ID;
-using Terraria.DataStructures;
-using Microsoft.Xna.Framework;
 
-namespace WaterGuns
+namespace WaterGuns;
+
+public class WaterGuns : Mod
 {
-    public class WaterGuns : Mod
-    {
-        public class ProjectileData : IEntitySource
-        {
-            public string context = "";
-            public string Context { get { return context; } }
-            public ProjectileData(IEntitySource original)
-            {
-                context = original.Context;
-            }
 
-            public Color color = default;
-            public int dustAmount = 3;
-            public float dustScale = 1.2f;
-            public float fadeIn = 1;
-            public int alpha = 75;
-
-            // Effects
-            public bool hasBuff = false;
-            public int buffType = 0;
-            public int buffTime = 0;
-
-            public bool bounces = false;
-            public bool homesIn = false;
-            public bool spawnsStar = false;
-            public bool penetrates = false;
-
-            // Full charge
-            public bool fullCharge = false;
-        }
-    }
 }
