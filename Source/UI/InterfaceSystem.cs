64f4b0c46700322cc2acd5c38973fdd2efcd1698
ThreeDots <threedotsellipsis@protonmail.com>
2024-07-11 23:12:32 +0500

    Add gun template for golden gun

--- Source/UI/InterfaceSystem.cs
+++ Source/UI/InterfaceSystem.cs
@@ -1,6 +1,8 @@
 using System.Collections.Generic;
 using Microsoft.Xna.Framework;
 using Terraria;
+using Terraria.ID;
+using Terraria.Localization;
 using Terraria.ModLoader;
 using Terraria.UI;
 
@@ -43,13 +45,27 @@ class InterfaceSystem : ModSystem
         int mouseTextIndex = layers.FindIndex(layer => layer.Name.Equals("Vanilla: Mouse Text"));
         if (mouseTextIndex != -1)
         {
-            layers.Insert(mouseTextIndex, new LegacyGameInterfaceLayer("WaterGuns: Interface", delegate {
-                              if (_lastUpdateUI != null && _interface?.CurrentState != null)
-                              {
-                                  _interface.Draw(Main.spriteBatch, _lastUpdateUI);
-                              }
-                              return true;
-                          }, InterfaceScaleType.UI));
+            layers.Insert(mouseTextIndex, new LegacyGameInterfaceLayer("WaterGuns: Interface", delegate
+            {
+                if (_lastUpdateUI != null && _interface?.CurrentState != null)
+                {
+                    _interface.Draw(Main.spriteBatch, _lastUpdateUI);
+                }
+                return true;
+            }, InterfaceScaleType.UI));
         }
+    }
+
+    public override void AddRecipeGroups()
+    {
+        base.AddRecipeGroups();
+
+        RecipeGroup goldBars = new RecipeGroup(() => Language.GetTextValue("LegacyMisc.37") + " Gold Bar",
+                                               new int[] { ItemID.GoldBar, ItemID.PlatinumBar });
+        RecipeGroup.RegisterGroup("WaterGuns:GoldBar", goldBars);
+
+        RecipeGroup titaniumBars = new RecipeGroup(() => Language.GetTextValue("LegacyMisc.37") + " Titanium Bar",
+                                                   new int[] { ItemID.TitaniumBar, ItemID.AdamantiteBar });
+        RecipeGroup.RegisterGroup("WaterGuns:TitaniumBar", titaniumBars);
     }
 }
---
+++ Source/Weapons/Golden/GoldenGun.cs
@@ -0,0 +1,98 @@
+using System.Collections.Generic;
+using Microsoft.Xna.Framework;
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+using WaterGuns.Projectiles.Sea;
+using WaterGuns.Utils;
+using WaterGuns.Weapons.Modules;
+
+namespace WaterGuns.Weapons.Golden;
+
+public class GoldenGun : BaseGun
+{
+    public override string Texture => TexturesPath.Weapons + "GoldenGun";
+
+    public SoundModule Sound { get; private set; }
+    public PropertyModule Property { get; private set; }
+    public PumpModule Pump { get; private set; }
+
+    public GoldenGun() : base()
+    {
+        Sound = new SoundModule(this);
+        Property = new PropertyModule(this);
+        Pump = new PumpModule(this);
+    }
+
+    public override void SetDefaults()
+    {
+        base.SetDefaults();
+
+        Sound.SetWater(this);
+        Property.SetDefaults(this);
+        Property.SetProjectile<SeaProjectile>(this);
+
+        Sprite.HoldoutOffset = new Vector2(-12, 6);
+        Sprite.Offset = new Vector2(52f, 52f);
+        Pump.MaxPumpLevel = 12;
+        Property.Inaccuracy = 3.3f;
+
+        Item.width = 58;
+        Item.height = 40;
+        Item.damage = 12;
+        Item.knockBack = 1.8f;
+
+        Item.useTime = 20;
+        Item.useAnimation = 20;
+        Item.shootSpeed = 22f;
+    }
+
+    public override void HoldItem(Terraria.Player player)
+    {
+        base.HoldItem(player);
+
+        Pump.DefaultUpdate();
+
+        if(Main.mouseRight)
+        {
+            AltUseAlways(player);
+        }
+    }
+
+    public void AltUseAlways(Player player)
+    {
+        if (Pump.Pumped)
+        {
+            Pump.Reset();
+        }
+    }
+
+    public override bool Shoot(Terraria.Player player, Terraria.DataStructures.EntitySource_ItemUse_WithAmmo source,
+                               Microsoft.Xna.Framework.Vector2 position, Microsoft.Xna.Framework.Vector2 velocity,
+                               int type, int damage, float knockback)
+    {
+        base.Shoot(player, source, position, velocity, type, damage, knockback);
+
+        position = Sprite.ApplyOffset(position, velocity);
+        velocity = Property.ApplyInaccuracy(velocity);
+
+        ShootProjectile<SeaProjectile>(player, source, position, velocity, damage, knockback);
+
+        return false;
+    }
+
+    public override void AddRecipes()
+    {
+        Recipe recipe = CreateRecipe();
+        recipe.AddRecipeGroup("WaterGuns:GoldBar", 10);
+        recipe.AddTile(TileID.Anvils);
+        recipe.Register();
+    }
+
+    public override void ModifyTooltips(List<TooltipLine> tooltip)
+    {
+        base.ModifyTooltips(tooltip);
+
+        Sprite.AddAmmoTooltip(tooltip, Mod);
+    }
+}
