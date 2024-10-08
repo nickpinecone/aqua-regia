3774f3a1b17dab00e91fc94213003cd73282e2bc
nickpinecone <nick.pineconefir@gmail.com>
2024-10-08 22:48:48 +0500

    Stubs for coral reef biome

---
+++ Source/World/CoralReef.cs
@@ -0,0 +1,24 @@
+using AquaRegia.Utils;
+using Terraria;
+using Terraria.ModLoader;
+
+namespace AquaRegia.World;
+
+public class CoralReef : ModBiome
+{
+    public override void OnEnter(Player player)
+    {
+        ChatLog.Message("Player has entered the coral reef biome!");
+    }
+
+    public override bool IsBiomeActive(Player player)
+    {
+        // If we are on the left ocean
+        return ((player.Center.X < WorldGen.oceanDistance * 16 ||
+                 // Or on the right ocean
+                 player.Center.X > (Main.maxTilesX - WorldGen.oceanDistance) * 16) &&
+                // And we are below ocean level
+                player.Center.Y > WorldGen.oceanLevel * 16);
+        // Then check for at lest x amount of coral tiles
+    }
+}
--- Source/World/TestGen.cs
+++ Source/World/TestGen.cs
@@ -3,8 +3,10 @@ using Terraria.ModLoader;
 using Microsoft.Xna.Framework.Input;
 using Terraria.ID;
 using Microsoft.Xna.Framework;
+using AquaRegia.Utils;
+using Terraria.GameContent.Personalities;
 
-namespace World;
+namespace AquaRegia.World;
 
 public class TestGen : ModSystem
 {
@@ -15,16 +17,48 @@ public class TestGen : ModSystem
 
     public override void PostUpdateWorld()
     {
+        if (JustPressed(Keys.D9))
+        {
+            DebugInfo();
+        }
+
         if (JustPressed(Keys.D0))
         {
             TestMethod((int)Main.MouseWorld.X / 16, (int)Main.MouseWorld.Y / 16);
         }
     }
 
+    private void DebugInfo()
+    {
+        ChatLog.Message(Main.LocalPlayer.Center.ToTileCoordinates().ToVector2(), "Player Center: ");
+        ChatLog.Message(WorldGen.oceanDistance.ToString());
+        ChatLog.Message(Main.maxTilesX.ToString());
+        ChatLog.Message((Main.maxTilesX - WorldGen.oceanDistance).ToString());
+        var ocean = new OceanBiome();
+        ChatLog.Message(ocean.IsInBiome(Main.LocalPlayer).ToString());
+
+        ChatLog.Message(WorldGen.oceanLevel.ToString());
+    }
+
     private void TestMethod(int x, int y)
     {
         Dust.QuickBox(new Vector2(x, y) * 16, new Vector2(x + 1, y + 1) * 16, 2, Color.YellowGreen, null);
 
-        WorldGen.TileRunner(x - 1, y, WorldGen.genRand.Next(3, 8), WorldGen.genRand.Next(2, 8), TileID.CobaltBrick);
+        // Only approximately, near the place where water starts
+        // var oceanStartX = Main.maxTilesX - WorldGen.oceanDistance;
+
+        // Again, also very approximate
+        var oceanCenterX = Main.maxTilesX - WorldGen.oceanDistance / 3f;
+        // var oceanCenterX = x;
+
+        // TODO actually calculate the ocean center, cause above could be a sky island?
+        // var oceanCenterY = 0;
+        var oceanCenterY = WorldGen.oceanLevel - 200;
+
+        // Dig tunnel is inconsistent and makes for weird generations
+        // Unless i figure this out, probably have to make an hand-made algorithm using WorldGen.KillTile
+        WorldGen.digTunnel(oceanCenterX, oceanCenterY, 0.5, 1, 1000, (int)(WorldGen.oceanDistance / 2f));
+
+        // WorldGen.TileRunner(x - 1, y, WorldGen.genRand.Next(3, 8), WorldGen.genRand.Next(2, 8), TileID.CobaltBrick);
     }
 }
