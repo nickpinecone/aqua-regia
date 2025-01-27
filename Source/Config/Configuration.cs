cfd30eab4b487c1a81abd5a07022e73b1b1de3e6
nickpinecone <nick.pineconefir@gmail.com>
2025-01-27 13:31:27 +0500

    Bring back config and utils

---
+++ Source/Config/Configuration.cs
@@ -0,0 +1,23 @@
+using System.ComponentModel;
+using Terraria.ModLoader.Config;
+
+namespace AquaRegia.Config;
+
+#pragma warning disable 612, 618
+public class Configuration : ModConfig
+{
+    public override ConfigScope Mode => ConfigScope.ServerSide;
+
+    [Header("WorldGeneration")]
+    [Label("Generate Coral Reef (Experimental)")]
+    [DefaultValue(false)]
+    public bool CoralReefGenEnabled { get; set; }
+
+    [Header("Debug")]
+    [Label("Enable Debug Info")]
+    [DefaultValue(false)]
+    public bool DebugInfoEnabled {
+        get; set;
+    }
+}
+#pragma warning restore 612, 618
