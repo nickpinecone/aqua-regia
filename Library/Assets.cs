484fe9168e4eace5a52cb2af5d2718c2dbce5dc8
nickpinecone <nick.pineconefir@gmail.com>
2025-08-24 21:28:12 +0500

    Initiate refactor

---
+++ Library/Assets.cs
@@ -0,0 +1,11 @@
+namespace AquaRegia.Library;
+
+// TODO restore with TypedPath nuget package
+// [TypedPath("Assets")]
+// public class Assets : ITypedPath
+// {
+//     public static string Wrap(string path)
+//     {
+//         return "AquaRegia/" + path;
+//     }
+// }
